5ed6961b5ed78729533aa4fa603271c16c4db62a
benjmhart <benjmhart@gmail.com>
2019-03-06 10:37:01 -0500

    stabilized halogen ajax example to actually work

--- public/src/Component.purs
+++ public/src/Component.purs
@@ -26,7 +26,7 @@ type State =
 
 data Query a
   = SetUsername String a
-  | MakeRequest Event a
+  | MakeRequest a
 
 ui :: H.Component HH.HTML Query Unit Void Aff
 ui =
@@ -43,7 +43,7 @@ ui =
 
   render :: State -> H.ComponentHTML Query
   render st =
-    HH.form_ [ HE.onSubmit (Just <<< MakeRequest)] $
+    HH.div_ $
       [ HH.h1_ [ HH.text "Lookup GitHub user" ]
       , HH.label_
           [ HH.div_ [ HH.text "Enter username:" ]
@@ -54,6 +54,7 @@ ui =
           ]
       , HH.button
           [ HP.disabled st.loading
+          , HE.onClick (HE.input_ MakeRequest)
           ]
           [ HH.text "Fetch info" ]
       , HH.p_
@@ -74,8 +75,7 @@ ui =
     SetUsername username next -> do
       H.modify_ (_ { username = username, result = Nothing :: Maybe String })
       pure next
-    MakeRequest event next -> do
-      H.liftEffect $ Event.preventDefault event
+    MakeRequest next -> do
       username <- H.gets _.username
       H.modify_ (_ { loading = true })
       response <- H.liftAff $ AX.get AXRF.string ("https://api.github.com/users/" <> username)
