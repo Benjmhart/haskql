f37e4ee8ef460e381a27cf5ff67c86d433a0d001
benjmhart <benjmhart@gmail.com>
2019-03-14 20:48:24 -0400

    GQL rough-in:  return 1 anonymous GQL Query

--- hastock/src/Handler/Home.hs
+++ hastock/src/Handler/Home.hs
@@ -4,10 +4,20 @@
 {-# LANGUAGE MultiParamTypeClasses  #-}
 {-# LANGUAGE TypeFamilies           #-}
 {-# LANGUAGE QuasiQuotes            #-}
+{-# LANGUAGE TypeApplications       #-}
+{-# LANGUAGE TypeOperators          #-}
+{-# LANGUAGE DataKinds              #-}
+
 module Handler.Home where
 
 import Import
 import Network.HTTP.Simple
+import Data.Text (Text)
+import Data.Monoid ((<>))
+import qualified GraphQL as GQL
+import qualified GraphQL.API as GQLAPI
+import GraphQL.API ((:>))
+import qualified GraphQL.Resolver as GQLR -- we mostly care about Handler
 
 getHomeR :: HandlerFor App Value
 getHomeR  = return $ object ["symbol" .= ("Hello World" :: Value)]
@@ -23,3 +33,19 @@ getStocksQuoteR symbol = do
                                         requestURL
     response <- httpJSON $ request
     return $ getResponseBody response
+
+type Hello = GQLAPI.Object "Hello" '[]
+  '[ GQLAPI.Argument "who" Text :> GQLAPI.Field "greeting" Text ]
+
+hello :: GQLR.Handler IO Hello
+hello = pure (\who -> pure ("Hello " <> who))
+
+
+-- postHelloGraphR = GQL.interpretAnonymousQuery @Hello hello
+postHelloGraphR :: Handler Value
+postHelloGraphR = do
+  body <- requireJsonBody :: Handler Value
+  print body
+  let res =  GQL.interpretAnonymousQuery @Hello hello "{ greeting(who: \"oscar\") }"
+  resJson <- liftIO $ toJSON <$> res
+  return resJson
