f0063f9ed09472790725b62785971e0289a3f323
Jonathan Lorimer <jonathan_lorimer@mac.com>
2019-04-23 20:03:10 -0400

    began work on registration route

--- hastock/src/Handler/Home.hs
+++ hastock/src/Handler/Home.hs
@@ -11,6 +11,12 @@ import Import
 
 import Handler.Quote.GetQuote (getQuote)
 import Model.User
+import Model.User             (UnvalidatedUser(..))
+import Database.Persist.Class (insert)
+import Data.Aeson             (fromJSON)
+import Data.Aeson
+import Data.Text as T
+import Data.Char
 
 getHomeR :: Handler ()
 getHomeR = sendFile "text/html" "static/index.html"
@@ -29,4 +35,58 @@ getInsertR = \stockSymbol -> do
   liftIO $ print result
   return $ toJSON ("" :: String)
 
+postRegisterR :: HandlerFor App ()
+postRegisterR = do
+    body <- requireJsonBody :: Handler Value
+    let unvalidatedUser = fromJSON body :: Result UnvalidatedUser
+    let validatedUser = passwordValidator =<< emailValidator =<< nameValidator =<< unvalidatedUser
+    print validatedUser
+  -- Encrypt Password
+  -- Insert
+  -- Generate JWT
+  -- Return JWT
 
+weirdChars :: Char -> Bool
+weirdChars char = T.any (== char) ";/\\(){}[]"
+
+nameValidator :: UnvalidatedUser -> Result UnvalidatedUser
+nameValidator user@(UnvalidatedUser name' _ _)
+  | notLongerThan3 = Error "name needs to be longer than 3 characters"
+  | hasWhitespace = Error "name cannot contain only white space"
+  | hasWeirdChars = Error "suspicious characters in name"
+  | otherwise = Success user
+    where
+      notLongerThan3 = not . (3<) . T.length $ name'
+      hasWhitespace = (T.any (== ' ')) $ name'
+      hasWeirdChars = T.any weirdChars name'
+
+emailValidator :: UnvalidatedUser -> Result UnvalidatedUser
+emailValidator user@(UnvalidatedUser _ email' _)
+  | notLongerThan3 = Error "email needs to be longer than 3 characters"
+  | hasWhitespace = Error "email cannot contain only white space"
+  | hasWeirdChars = Error "suspicious characters in email"
+  | not hasAt = Error "needs a single @ sign in email"
+  | not hasDots = Error "needs a . in email"
+  | otherwise = Success user
+    where
+      notLongerThan3 = not . (3<) . T.length $ email'
+      hasWhitespace = (T.any (== ' ')) $ email'
+      hasWeirdChars = T.any weirdChars email'
+      hasAt = (== 1) . T.length . (T.filter (== '@')) $ email'
+      hasDots = T.any (== '.') $ email'
+
+passwordValidator :: UnvalidatedUser -> Result UnvalidatedUser
+passwordValidator user@(UnvalidatedUser _ _ password')
+  | notLongerThan3 = Error "password needs to be longer than 3 characters"
+  | hasWhitespace = Error "password cannot contain only white space"
+  | hasWeirdChars = Error "suspicious characters in password"
+  | hasCorrectCharacters = Error "password needs uppercase, lowercase, special, and digit"
+  | otherwise = Success user
+    where
+      notLongerThan3 = not . (6<) . T.length $ password'
+      hasWhitespace = (T.any (== ' ')) $ password'
+      hasWeirdChars = T.any weirdChars password'
+      hasCorrectCharacters = not
+                           . and 
+                           $ ((flip T.any) password')
+                           <$> [isUpper, isLower, isPunctuation, isDigit]
