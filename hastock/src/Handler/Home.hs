32c7f6a58802c9197ad5dd05a19bbe326dd1ab9e
benjmhart <benjmhart@gmail.com>
2019-05-13 20:59:29 -0400

    encrypt password and store in db

--- hastock/src/Handler/Home.hs
+++ hastock/src/Handler/Home.hs
@@ -7,17 +7,16 @@
 
 module Handler.Home where
 
-import Import
+import           Import
 
-import Handler.Quote.GetQuote (getQuote)
-import Model.User
-import Model.User             (UnvalidatedUser(..))
-import Database.Persist.Class (insert)
-import Data.Aeson             (fromJSON)
-import Data.Aeson
-import Data.Text as T
-import Data.Char
-import Yesod.Auth.Util.PasswordStore
+import           Handler.Quote.GetQuote         ( getQuote )
+import           Model.User
+import           Model.User                     ( UnvalidatedUser(..) )
+import           Data.Aeson                     ( fromJSON )
+import           Data.Aeson
+import           Data.Text                     as T
+import           Data.Char
+import           Yesod.Auth.Util.PasswordStore  ( makePassword )
 -- import Crypto.KDF.BCrypt
 
 getHomeR :: Handler ()
@@ -39,20 +38,43 @@ getInsertR = \stockSymbol -> do
 
 postRegisterR :: HandlerFor App ()
 postRegisterR = do
-    body <- requireJsonBody :: Handler Value
-    let unvalidatedUser = fromJSON body :: Result UnvalidatedUser
-    let validatedUser = passwordValidator =<< emailValidator =<< nameValidator =<< unvalidatedUser
-    let hashedPassword = ((flip makePassword) 10 . encodeUtf8 . password) <$> validatedUser
-    let validatedUser' = (liftM2 makeValidatedUser) validatedUser hashedPassword
-    print validatedUser'
-  -- Encrypt Password
+  body <- requireJsonBody :: Handler Value
+  let unvalidatedUser = fromJSON body :: Result UnvalidatedUser
+  let validatedUser =
+        passwordValidator
+          =<< emailValidator
+          =<< nameValidator
+          =<< unvalidatedUser
+  case validatedUser of
+    (Error str) -> do
+      print "wrong wrong wrong"
+      putStrLn . T.pack $ str
+      return ()
+    (Success vu) -> do
+      hashedPassword <-
+        liftIO $ ((flip makePassword) 10 . encodeUtf8 . password) $ vu
+      print hashedPassword
+      let validatedUser' = (flip makeValidatedUser $ hashedPassword) $ vu
+      -- We Can't do inserts using insertKey  because it breaks stuff
+      key <- runDB $ insert validatedUser'
+      --this successfully updates the DB
+      --NEXT STEP :  clean up hash, JWT, clean up response and error values
+      -- next next step:   be good at haskell
+
+      print "yippie!"
+      print key
+
+      return ()
+
+
+
   -- Insert
   -- Generate JWT
   -- Return JWT
 
-makeValidatedUser :: UnvalidatedUser -> ByteString -> Result User
-makeValidatedUser (UnvalidatedUser name email _) hashedPassword
-  = Success $ User name email $ decodeUtf8 hashedPassword 
+makeValidatedUser :: UnvalidatedUser -> ByteString -> User
+makeValidatedUser (UnvalidatedUser name email _) hashedPassword =
+  User name email $ decodeUtf8 hashedPassword
 
 weirdChars :: Char -> Bool
 weirdChars char = T.any (== char) ";/\\(){}[]"
@@ -60,41 +82,43 @@ weirdChars char = T.any (== char) ";/\\(){}[]"
 nameValidator :: UnvalidatedUser -> Result UnvalidatedUser
 nameValidator user@(UnvalidatedUser name' _ _)
   | notLongerThan3 = Error "name needs to be longer than 3 characters"
-  | hasWhitespace = Error "name cannot contain only white space"
-  | hasWeirdChars = Error "suspicious characters in name"
-  | otherwise = Success user
-    where
-      notLongerThan3 = not . (3<) . T.length $ name'
-      hasWhitespace = (T.any (== ' ')) $ name'
-      hasWeirdChars = T.any weirdChars name'
+  | hasWhitespace  = Error "name cannot contain only white space"
+  | hasWeirdChars  = Error "suspicious characters in name"
+  | otherwise      = Success user
+ where
+  notLongerThan3 = not . (3 <) . T.length $ name'
+  hasWhitespace  = (T.any (== ' ')) $ name'
+  hasWeirdChars  = T.any weirdChars name'
 
 emailValidator :: UnvalidatedUser -> Result UnvalidatedUser
 emailValidator user@(UnvalidatedUser _ email' _)
   | notLongerThan3 = Error "email needs to be longer than 3 characters"
-  | hasWhitespace = Error "email cannot contain only white space"
-  | hasWeirdChars = Error "suspicious characters in email"
-  | not hasAt = Error "needs a single @ sign in email"
-  | not hasDots = Error "needs a . in email"
-  | otherwise = Success user
-    where
-      notLongerThan3 = not . (3<) . T.length $ email'
-      hasWhitespace = (T.any (== ' ')) $ email'
-      hasWeirdChars = T.any weirdChars email'
-      hasAt = (== 1) . T.length . (T.filter (== '@')) $ email'
-      hasDots = T.any (== '.') $ email'
+  | hasWhitespace  = Error "email cannot contain only white space"
+  | hasWeirdChars  = Error "suspicious characters in email"
+  | not hasAt      = Error "needs a single @ sign in email"
+  | not hasDots    = Error "needs a . in email"
+  | otherwise      = Success user
+ where
+  notLongerThan3 = not . (3 <) . T.length $ email'
+  hasWhitespace  = (T.any (== ' ')) $ email'
+  hasWeirdChars  = T.any weirdChars email'
+  hasAt          = (== 1) . T.length . (T.filter (== '@')) $ email'
+  hasDots        = T.any (== '.') $ email'
 
 passwordValidator :: UnvalidatedUser -> Result UnvalidatedUser
 passwordValidator user@(UnvalidatedUser _ _ password')
   | notLongerThan3 = Error "password needs to be longer than 3 characters"
   | hasWhitespace = Error "password cannot contain only white space"
   | hasWeirdChars = Error "suspicious characters in password"
-  | hasCorrectCharacters = Error "password needs uppercase, lowercase, special, and digit"
+  | hasCorrectCharacters = Error
+    "password needs uppercase, lowercase, special, and digit"
   | otherwise = Success user
-    where
-      notLongerThan3 = not . (6<) . T.length $ password'
-      hasWhitespace = (T.any (== ' ')) $ password'
-      hasWeirdChars = T.any weirdChars password'
-      hasCorrectCharacters = not
-                           . and 
-                           $ ((flip T.any) password')
-                           <$> [isUpper, isLower, isPunctuation, isDigit]
+ where
+  notLongerThan3 = not . (6 <) . T.length $ password'
+  hasWhitespace  = (T.any (== ' ')) $ password'
+  hasWeirdChars  = T.any weirdChars password'
+  hasCorrectCharacters =
+    not
+      .   and
+      $   ((flip T.any) password')
+      <$> [isUpper, isLower, isPunctuation, isDigit]
